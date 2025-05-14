45a9e05f4c3f28e395caedce94d70932b2792a26
Edgar <edgarseabra@outlook.com.br>
2025-05-14 16:02:53 -0300

    📙 - Helper and Config files

--- src/js/model.js
+++ src/js/model.js
@@ -1,21 +1,15 @@
+import { API_URL } from './config';
+import { getJSON } from './helper';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
-    );
-
-    const data = await res.json();
-
-    if (!res.ok) {
-      throw new Error(`${data.message} (${res.status})`);
-    }
+    const data = await getJSON(API_URL + '/' + id);
 
     const { recipe } = data.data;
-
     state.recipe = {
       id: recipe.id,
       title: recipe.title,
@@ -28,6 +22,6 @@ export const loadRecipe = async function (id) {
     };
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    console.error(err) + '🧨🧨🧨';
   }
 };
