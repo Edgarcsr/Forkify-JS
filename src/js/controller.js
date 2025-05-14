1c6fcb481fd6d100e79c99e44e4d7d79fd2596a0
Edgarcsr <edgarseabra@outlook.com.br>
2025-05-14 11:27:50 -0300

    📙 - Hash change events

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,13 +30,15 @@ const renderSipnner = function (parentEl) {
 };
 
 const showRecipe = async function () {
+  const id = window.location.hash.slice(1);
+
   try {
+    if (!id) return;
+
     // 1) Loading Recipe
     renderSipnner(recipeContainer);
     const res = await fetch(
-      // `https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e8297`
-      `https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886`
-      // `https://forkify-api.jonas.io/api/v2/recipes`
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
     );
 
     const data = await res.json();
@@ -166,4 +168,6 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'laod'].forEach(ev => window.addEventListener(ev, showRecipe));
+
+// window.addEventListener('hashchange', showRecipe);
