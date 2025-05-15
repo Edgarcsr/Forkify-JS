dbade5475fdbbd20d991ad415f08193876b266bb
Edgar <edgarseabra@outlook.com.br>
2025-05-15 17:42:36 -0300

    📙 - Start to implement search feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,8 @@
 import * as model from '../js/model';
 import recipeView from './views/recipeView';
+import searchView from './views/searchView';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-
-const recipeContainer = document.querySelector('.recipe');
 
 // NEW API URL (instead of the one shown in the video)
 // https://forkify-api.jonas.io
@@ -27,8 +26,23 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    // 1. Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2. Load search results
+    await model.loadSearchResult(query);
+    // 3. Render ersult
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
